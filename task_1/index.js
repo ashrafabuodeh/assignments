c5b72069d122dae2763b66aece4302a0b1373435
ashrafabuodeh <ashraf.abuodeh99@gmail.com>
2021-03-22 21:39:53 +0200

    task1 part2 update 6

--- task_1/index.js
+++ task_1/index.js
@@ -1,11 +1,12 @@
 const onMenueItemClick = (elementId)=>{
-     const x =document.getElementById(elementId) ;
+     const itemClick = document.getElementById(elementId) ;
     const NumberOfItem = document.getElementsByClassName('Nav-items').length;
-         for(let i = 0 ; i<NumberOfItem;i++){
+         for(let i = 0 ; i < NumberOfItem; i++){
             if(document.getElementsByClassName('Nav-items')[i].classList.contains('active')){
                 document.getElementsByClassName('Nav-items')[i].classList.remove('active');
          }
         }
         document.getElementById(elementId).classList.add('active');       
     } 
+    
     

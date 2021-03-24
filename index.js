33cba00fef390cdaeb22688bdff788a719874be1
ashrafabuodeh <ashraf.abuodeh99@gmail.com>
2021-03-24 16:25:27 +0200

    task2 form validation update7

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const email = document.getElementById("email");
-const companyName = document.getElementById("companyName");
-const password = document.getElementById("password1");
-const passwordCheck = document.getElementById("password2");
+const companyName = document.getElementById("company-name");
+const password = document.getElementById("password");
+const passwordCheck = document.getElementById("password-check");
 const city = document.getElementById("city");
 function checkValidate(e) {
     e.preventDefault();
     let check = true;
     const regularExpressionForEmail = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/;
     if (!regularExpressionForEmail.test(email.value)) {
-        document.getElementById("emailError").innerHTML = "Email is not valid";
+        document.getElementById("email-error").innerHTML = "Email is not valid";
         check = false;
     } else {
-        document.getElementById("emailError").innerHTML = "";
+        document.getElementById("email-error").innerHTML = "";
     }
     if (companyName.value === "") {
-        document.getElementById("companyError").innerHTML = "Company name can't be empty";
+        document.getElementById("company-error").innerHTML = "Company name can't be empty";
         check = false;
     } else {
-        document.getElementById("companyError").innerHTML = "";
+        document.getElementById("company-error").innerHTML = "";
     }
     const regularExpressionForPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/;
     if (!regularExpressionForPassword.test(password.value)) {
-        document.getElementById("passwordError").innerHTML = "Password must have 8 or more characters with a mix of letters, numbers & symbols.";
+        document.getElementById("password-error").innerHTML = "Password must have 8 or more characters with a mix of letters, numbers & symbols.";
         check = false;
     } else {
-        document.getElementById("passwordError").innerHTML = "";
+        document.getElementById("password-error").innerHTML = "";
         if (password.value !== passwordCheck.value) {
-            document.getElementById("passwordCheckError").innerHTML = "Passwords don't match";
+            document.getElementById("password-check-error").innerHTML = "Passwords don't match";
             check = false;
         } else {
-            document.getElementById("passwordCheckError").innerHTML = "";
+            document.getElementById("password-check-error").innerHTML = "";
         }
     }
     if (city.value === "Choose Your City") {
-        document.getElementById("optionError").innerHTML = "please select a City ";
+        document.getElementById("option-error").innerHTML = "please select a City ";
         check = false;
     } else {
-        document.getElementById("optionError").innerHTML = "";
+        document.getElementById("option-error").innerHTML = "";
     }
     if(check)
     adduser(email, companyName, password, city);
@@ -58,7 +58,7 @@ function adduser(email, companyName, password, city) {
         }
         else
         {
-            document.getElementById("emailError").innerHTML = "This email is exist";
+            document.getElementById("email-error").innerHTML = "This email is exist";
         }
     }
 }

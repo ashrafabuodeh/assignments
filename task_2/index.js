4c5177ab1e8b3c7279118399c23d7322e13167ad
ashrafabuodeh <ashraf.abuodeh99@gmail.com>
2021-04-01 09:52:49 +0300

    Re-design the layout and CSSDesign

--- task_2/index.js
+++ task_2/index.js
@@ -1,13 +1,18 @@
-const email = document.getElementById("email");
+const city = document.getElementById("city");
 const companyName = document.getElementById("company-name");
+const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordCheck = document.getElementById("password-check");
-const city = document.getElementById("city");
+const regularExpressionForEmail = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/;
+const regularExpressionForPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
 
 checkValidate = (e) => {
     e.preventDefault();
     let check = true;
-    const regularExpressionForEmail = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/;
+    if (email.value === '') {
+        document.getElementById("email-error").innerHTML = "Please enter an email";
+        check = false;
+    } else
     if (!regularExpressionForEmail.test(email.value)) {
         document.getElementById("email-error").innerHTML = "Email is not valid";
         check = false;
@@ -20,9 +25,8 @@ checkValidate = (e) => {
     } else {
         document.getElementById("company-error").innerHTML = "";
     }
-    const regularExpressionForPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/;
     if (!regularExpressionForPassword.test(password.value)) {
-        document.getElementById("password-error").innerHTML = "Password must have 8 or more characters with a mix of letters, numbers & symbols.";
+        document.getElementById("password-error").innerHTML = "Password must contains a mix of letters, numbers & symbols";
         check = false;
     } else {
         document.getElementById("password-error").innerHTML = "";
@@ -40,21 +44,33 @@ checkValidate = (e) => {
         document.getElementById("option-error").innerHTML = "";
     }
     if (check)
-        adduser(email, companyName, password, city);
+    {
+        const user = new User(email , companyName , password , city);
+        addUser(user);
+    }
 }
 
-adduser = (email, companyName, password, city) => {
-    const user = {
-        email: email.value,
-        companyName: companyName.value,
-        password1: password.value,
-        city: city.value,
-    };
-    const convertToJSON = JSON.stringify(user);
-    if (localStorage.getItem(email.value) === null) {
-        localStorage.setItem(email.value, convertToJSON);
+addUser = user => {
+    const convertToJSON = JSON.stringify(user.personInfo);
+    if (localStorage.getItem(user.personInfo.email) === null) {
+        localStorage.setItem(user.personInfo.email, convertToJSON);
         location.replace("../task_1/index.html");   
     } else {
-        document.getElementById("email-error").innerHTML = "Email is exist";
+        document.getElementById("email-error").innerHTML = "This email already exists";
+    }
+}
+
+class User{
+    personInfo = {
+        email: '',
+        companyName: '',
+        password: '',
+        city: ''
+    };
+    constructor(email , companyName , password , city){
+        this.personInfo.email = email.value ;
+        this.personInfo.companyName = companyName.value,
+        this.personInfo.password = password.value ,
+        this.personInfo.city = city.value
     }
 }
